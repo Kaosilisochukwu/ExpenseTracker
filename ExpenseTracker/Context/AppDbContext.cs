cbc646b9a30988485dec9e7398105cbab266ed1b
Kaosilisochukwu Nwizu <kcharlse94@gmail.com>
2020-12-11 20:59:51 +0100

    Add database context

---
+++ ExpenseTracker/Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ExpenseTracker.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Context
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base (options) { }
+
+        public DbSet<Expense> Expenses { get; set; }
+    }
+}
