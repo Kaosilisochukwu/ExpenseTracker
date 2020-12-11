70e9bf97f0fa1e36eea9dd145852ec03372dbe84
Kaosilisochukwu Nwizu <kcharlse94@gmail.com>
2020-12-11 20:58:26 +0100

    migrate to database

---
+++ ExpenseTracker/Migrations/20201210153149_addRules.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ExpenseTracker.Migrations
+{
+    public partial class addRules : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Expenses_AspNetUsers_UserId1",
+                table: "Expenses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Expenses_UserId1",
+                table: "Expenses");
+
+            migrationBuilder.DropColumn(
+                name: "UserId1",
+                table: "Expenses");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Expenses",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Expenses",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Expenses_UserId",
+                table: "Expenses",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expenses_AspNetUsers_UserId",
+                table: "Expenses",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Expenses_AspNetUsers_UserId",
+                table: "Expenses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Expenses_UserId",
+                table: "Expenses");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Expenses",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Expenses",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId1",
+                table: "Expenses",
+                type: "varchar(255) CHARACTER SET utf8mb4",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Expenses_UserId1",
+                table: "Expenses",
+                column: "UserId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expenses_AspNetUsers_UserId1",
+                table: "Expenses",
+                column: "UserId1",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
