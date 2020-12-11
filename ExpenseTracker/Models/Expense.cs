d432ff1954c95cc2835de8de9884d6e433057ba0
Kaosilisochukwu Nwizu <kcharlse94@gmail.com>
2020-12-11 20:58:01 +0100

    add models for app

---
+++ ExpenseTracker/Models/Expense.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExpenseTracker.Models
+{
+    public class Expense
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please give a brief description of this transaction")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Please give the amount of this transaction")]
+        public decimal Amount { get; set; }
+
+        [Required(ErrorMessage = "Please add the vat for this tranaction")]
+        public decimal VAT { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        [Required(ErrorMessage = "Every transaction must have a user Id")]
+        public string UserId { get; set; }
+        public ApplicationUser User { get; set; }
+    }
+}
