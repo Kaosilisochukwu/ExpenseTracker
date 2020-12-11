6e283b88ba8e80dd0b8d0c4be33611d414db070d
Kaosilisochukwu Nwizu <kcharlse94@gmail.com>
2020-12-11 20:59:27 +0100

    Add Token generator

---
+++ ExpenseTracker/Utils/TokenConfiguration.cs
@@ -0,0 +1,48 @@
+﻿using ExpenseTracker.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ExpenseTracker.Utils
+{
+    public class TokenConfiguration
+    {
+        public static string GenerateToken(ApplicationUser user, IConfiguration config, List<string> roles = null)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim("userName", user.UserName)
+            };
+            if(roles != null)
+            {
+                foreach (var role in roles)
+                {
+                    claims.Add(new Claim(ClaimTypes.Role, role));
+                }
+            }
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config.GetSection("Jwt:SigninKey").Value));
+
+            var credential = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                SigningCredentials = credential,
+                Expires = DateTime.Now.AddDays(1)
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
