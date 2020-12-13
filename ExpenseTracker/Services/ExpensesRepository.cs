896cd0acf11ff756ddda51573d33c79e19332518
Kaosilisochukwu Nwizu <kcharlse94@gmail.com>
2020-12-13 19:59:53 +0100

    Add repository for expenses

---
+++ ExpenseTracker/Services/ExpensesRepository.cs
@@ -0,0 +1,44 @@
+﻿using ExpenseTracker.Context;
+using ExpenseTracker.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Services
+{
+    public class ExpensesRepository : IExpensesRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ExpensesRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Expense> GetExpenseById(int expenseId)
+        {
+            return await _context.Expenses.FirstOrDefaultAsync(expense => expense.Id == expenseId);
+        }
+
+        public async Task<IEnumerable<Expense>> GetExpenses()
+        {
+            return await _context.Expenses.ToListAsync();
+        }
+
+        public async Task<IEnumerable<Expense>> GetExpensesByUserId(string userId)
+        {
+            return await _context.Expenses.Where(expense => expense.UserId == userId).ToListAsync();
+        }
+
+        public async Task<int> SaveTransaction(Expense expense)
+        {
+            var numOfAffectedRows = 0;
+            var go = _context.Expenses.Add(expense);
+            if(go.State == EntityState.Added)
+            {
+                numOfAffectedRows = await _context.SaveChangesAsync();
+            }
+            return numOfAffectedRows;
+        }
+    }
+}
---
+++ ExpenseTracker/Services/IExpensesRepository.cs
@@ -0,0 +1,17 @@
+﻿using ExpenseTracker.DTOs;
+using ExpenseTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Services
+{
+    public interface IExpensesRepository
+    {
+        Task<IEnumerable<Expense>> GetExpenses();
+        Task<Expense> GetExpenseById(int expenseId);
+        Task<IEnumerable<Expense>> GetExpensesByUserId(string userId);
+        Task<int> SaveTransaction(Expense transaction);
+    }
+}
