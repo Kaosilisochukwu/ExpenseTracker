151937289e88e188e7f29a5b9ba017ee25c73a0a
Kaosilisochukwu Nwizu <kcharlse94@gmail.com>
2020-12-20 23:47:21 +0100

    remove unnecessary variables

--- ExpenseTracker/DTOs/ExpenseToRegisterDTO.cs
+++ ExpenseTracker/DTOs/ExpenseToRegisterDTO.cs
@@ -6,7 +6,6 @@ namespace ExpenseTracker.DTOs
     public class ExpenseToRegisterDTO
     {
         [Required(ErrorMessage = "Please add a description for this expense")]
-       // [Range(3, 50, ErrorMessage = "Description must be from 3 to 50 characters")]
         public string Description { get; set; }
 
         [Required(ErrorMessage = "Please add an amount for this expense")]
@@ -17,8 +16,5 @@ namespace ExpenseTracker.DTOs
 
         [Required(ErrorMessage = "Date field is required")]
         public DateTime Date { get; set; }
-
-        //[Required(ErrorMessage = "User Id is required")]
-        //public string UserId { get; set; }
     }
 }
