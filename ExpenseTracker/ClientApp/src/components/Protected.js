658e92f2ac6105b08289ec41e0ef60a53b0c9110
Kaosilisochukwu Nwizu <kcharlse94@gmail.com>
2020-12-20 23:45:49 +0100

    Add Expenses protected route

---
+++ ExpenseTracker/ClientApp/src/components/Protected.js
@@ -0,0 +1,17 @@
+﻿import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
+
+
+const Protected = ({ component: Component, isLoggedIn, ...rest }) => {
+
+
+    return (
+        <Route {...rest} render = {(props) => (
+            isLoggedIn
+                ? <Component {...props} />
+                : <Redirect to="/login" />
+         )} />
+    )
+}
+
+export default Protected;
